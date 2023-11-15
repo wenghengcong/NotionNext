8ca6954d9614b21a3045db5e44d61b207d1e6a6d
Nemo <wenghengcong@icloud.com>
2023-11-15 20:46:48 +0800

    update config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,13 +1,14 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2016-03-20', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  HEO_NOTICE_BAR: [],
+  // HEO_NOTICE_BAR: [
+  //   { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+  //   { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  // ],
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
@@ -18,26 +19,24 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '我的作品', url: '/tag/作品' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门' },
+  HEO_HERO_CATEGORY_3: { title: '必看精选', url: '/tag/精选' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2018/04/28/22/03/tree-3358468_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '探索世界的iOS开发',
+    '🔍 充满求知欲',
+    '📚 喜欢看杂书',
+    '🧠 摸索认知与思维',
+    '🛠️ 工具解决方案爱好者'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/wenghengcong', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
